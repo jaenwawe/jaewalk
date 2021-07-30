9f1f9c08f15cef24a593f2558fb9dde46e00471e
jaenwawe <nwawe.jae@gmail.com>
2021-07-30 18:01:03 -0500

    Clicks Resume and Git Hub

---
+++ script.js
@@ -0,0 +1,13 @@
+const grandparent = document.querySelector(".grandparent")
+
+grandparent.addEventListener("click", e => {console.log("Grand Parent 1")
+window.open(th.name,'_blank');
+})
+
+/*
+function openTab(th)
+{
+    window.open(th.name,'_blank');
+}
+
+*/
